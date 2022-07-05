e9f6746e8ed79b1f0109a40d7986b81f1927a274
Ivan Torres <ivantorresmia27@gmail.com>
2022-07-04 21:33:20 -0700

    fixing file path

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ var mysql = require('mysql2');
 /* GET home page. */
 // If no API routes are hit, send the React app
 router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+  res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
 module.exports = router;
