6da87017288bb0927208ee532830a63eb646353f
Ivan Torres <ivantorresmia27@gmail.com>
2022-07-05 16:37:10 -0700

    testing client pt7

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,16 @@ var mysql = require('mysql2');
 const path = require("path");
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-});
-
-// router.use(function(req, res) {
+// router.get('/', function(req, res, next) {
 //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-// })
+// });
+
+router.use(function(req, res) {
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
+})
+
+router.get("/message", (req, res) => {
+  res.send({ response: "I am alive" }).status(200);
+});
 
 module.exports = router;
