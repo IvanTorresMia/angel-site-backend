3f8d61fafea8dbdfd9cd9ebb3e9d27e0978d9532
Ivan Torres <ivantorresmia27@gmail.com>
2022-07-05 16:34:57 -0700

    testing client pt6

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2');
-const { path } = require('../app');
+const path = require("path");
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.sendFile(path.join(__dirname, "../client/test.html"));
+  res.sendFile(path.join(__dirname, "../client/build/index.html"));
 });
 
 // router.use(function(req, res) {
