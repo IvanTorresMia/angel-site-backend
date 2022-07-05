0cb97e2980a8e427a3ae3b312616e592d62fccc4
Ivan Torres <ivantorresmia27@gmail.com>
2022-07-05 16:42:59 -0700

    testing client pt9

--- routes/index.js
+++ routes/index.js
@@ -8,12 +8,18 @@ const path = require("path");
 //   res.sendFile(path.join(__dirname, "../client/build/index.html"));
 // });
 
-router.use(function(req, res) {
-  res.sendFile(path.join(__dirname, "../client/build/index.html"));
-})
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// })
 
-router.get("/message", (req, res) => {
-  res.json({ response: "I am alive" })
-});
+// router.get("/message", (req, res) => {
+//   res.json({ response: "I am alive" })
+// });
+
+
+app.use(express.static('client/build'));
+app.get('*',(req,res)=>{
+    res.sendFile(path.resolve(__dirname,'client','build','index.html'))
+})
 
 module.exports = router;
