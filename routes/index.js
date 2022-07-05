4b6c44345cd0461fb70ac3f16cef975967d585c7
Ivan Torres <ivantorresmia27@gmail.com>
2022-07-04 21:45:44 -0700

    debugging

--- routes/index.js
+++ routes/index.js
@@ -1,10 +1,15 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2');
+const { path } = require('../app');
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
+// router.get('/', function(req, res, next) {
+//   res.json({data:  "hello world"});
+// });
+
+router.use(function(req, res) {
   res.json({data:  "hello world"});
-});
+})
 
 module.exports = router;
