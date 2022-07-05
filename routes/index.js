5064157d8f8c1161148816bb90c3f29497fd815e
Ivan Torres <ivantorresmia27@gmail.com>
2022-07-04 21:38:38 -0700

    fixing route again

--- routes/index.js
+++ routes/index.js
@@ -5,7 +5,7 @@ const path = require("path");
 
 /* GET home page. */
 // If no API routes are hit, send the React app
-router.use(function(req, res) {
+router.get("/", function(req, res) {
   res.sendFile(path.join(__dirname, "../../client/build/index.html"));
 });
 
