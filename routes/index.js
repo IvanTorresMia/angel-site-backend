26d36a63e65d2c1e9ee9324b1b0452863e0472cb
Ivan Torres <ivantorresmia27@gmail.com>
2022-07-04 21:36:26 -0700

    intalling new things

--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,7 @@
 var express = require('express');
 var router = express.Router();
 var mysql = require('mysql2');
+const path = require("path");
 
 /* GET home page. */
 // If no API routes are hit, send the React app
