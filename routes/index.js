3736a23a8cc943c3b22fc50c1739bf17a41851be
Ivan Torres <ivantorresmia27@gmail.com>
2022-07-05 16:13:48 -0700

    testing client pt3

--- routes/index.js
+++ routes/index.js
@@ -4,12 +4,12 @@ var mysql = require('mysql2');
 const { path } = require('../app');
 
 /* GET home page. */
-// router.get('/', function(req, res, next) {
-//   res.json({data:  "hello world"});
-// });
-
-router.use(function(req, res) {
+router.get('/', function(req, res, next) {
   res.sendFile(path.join(__dirname, "../client/build/index.html"));
-})
+});
+
+// router.use(function(req, res) {
+//   res.sendFile(path.join(__dirname, "../client/build/index.html"));
+// })
 
 module.exports = router;
